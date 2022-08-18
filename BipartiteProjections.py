3397e1a27f3fd83151fd41c04f96c19e1ea83d47
Nicole Nisbett <n.nisbett@leeds.ac.uk>
2022-08-18 11:38:24 +0100

    Bipartite network creation and projection scripts

--- BipartiteProjections.py
+++ BipartiteProjections.py
@@ -1,20 +1,22 @@
+#Creating bipartite projections. Need to run in ARC because too memory intensive. Only works for COP20 (uses 80G)
 import networkx as nx
 from networkx.algorithms import bipartite
 
-g = nx.Graph()
-user = []
-topic = []
+version_list = ["COP20", "COP21", "COP22", "COP23", "COP24", "COP25", "COP26"]
 
-with open("Trimester Bipartite/SepDec17.csv", "r") as f:
-    f.readline()
-    for l in f:
-        l = l.rstrip().split(",")
-        u = str(l[0])
-        t = str(l[3])
-        g.add_edge(u, t)
-        user.append(u)
-        topic.append(t)
+for i in version_list:
+    g = nx.Graph()
+    user = []
+    topic = []
+    with open(i + "Merged.csv", "r") as f:
+        f.readline()
+        for l in f:
+            l = l.rstrip().split(",")
+            u = str(l[1])
+            t = str(l[2])
+            g.add_edge(u, t)
+            user.append(u)
+            topic.append(t)
 
-
-Ngraph_user = bipartite.weighted_projected_graph(g, user)
-nx.write_edgelist(Ngraph_user, "Proj_SepDec17.csv")
+    Ngraph_user = bipartite.weighted_projected_graph(g, user)
+    nx.write_edgelist(Ngraph_user, i + "Projections.csv",  delimiter = ",")
